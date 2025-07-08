0a1a3f126dc2a7af3c3392f5b65104a42c0351a1
Onlygideon <stephengideon22@gmail.com>
2025-07-08 19:32:10 +0100

    add: update script and reload render server to avoid cooldown period

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import axios from 'axios';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  (async () => {
+    const url = 'https://fossapay-quote.onrender.com';
+    const interval = 300000;
+
+    async function reloadServer() {
+      try {
+        const response: { status: number } = await axios.get(url);
+        console.log(
+          `Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`,
+        );
+      } catch (error) {
+        const err = error as Error;
+        console.error(
+          `Error reloading at ${new Date().toISOString()}:`,
+          err.message,
+        );
+      }
+    }
+
+    setInterval(reloadServer, interval);
+  })();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
